bc19bfbdf8a6dd983bf9268dd8d64274c3c433fd
Jasur Mansurov <makej1318@gmail.com>
2024-12-15 23:33:25 +0500

    Normalize line endings

---
+++ p-seed/backend/controllers/ReportDevController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace backend\controllers;
+
+use common\controllers\RefController;
+use yii\filters\AccessControl;
+use Yii;
+use yii\web\Controller;
+use yii\data\ArrayDataProvider;
+use common\models\Order; // Заказы
+use common\models\Region; // Районы
+use common\models\Product; // Товары
+use common\models\User; 
+
+class ReportDevController extends RefController
+{
+    /**
+     * {@inheritdoc}
+     */
+
+}
